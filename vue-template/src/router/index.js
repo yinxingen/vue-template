dd5fa94db287d09b6237df17d0f6698d70e4d8eb
殷鑫根 <yinxingen@yinxingendeMBP.lan>
2018-10-31 22:10:50 +0800

    初始化vue模板

---
+++ vue-template/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import login from '@/views/login/login.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'login',
+      component: login
+    }
+  ]
+})
---
+++ vue-template/src/views/login/login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="login">
+    <p>login</p>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .login{
+    font-size: 18px;
+    color: aqua;
+  }
+</style>
